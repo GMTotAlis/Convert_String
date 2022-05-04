d6170fa693855fa0dec3411cd2f00f2a3aee6b7a
Aleks <kychkinalek@yandex.ru>
2022-05-05 05:52:14 +0900

    Change main.py

--- main.py
+++ main.py
@@ -16,10 +16,15 @@ def ConvertLine(curr_string):
 
 
 if __name__ == '__main__':
+	list_of_strings=[]
 	with open('input.txt', 'r') as f_re:
-		list_of_strings = f_re.readlines()
+		for read_string in f_re.readlines():
+			if (read_string[-1]=='\n'):
+
+				read_string = read_string[:-1]
+			list_of_strings.append(read_string)
 	with open('output.txt', 'w') as f_wr:
 		for curr_string in list_of_strings:
-			result_string = ConvertLine(curr_string[:-1])
-			print('"'+curr_string[:-1]+'"'+' => '+'"'+result_string+'"')
+			result_string = ConvertLine(curr_string)
+			print('"'+curr_string+'"'+' => '+'"'+result_string+'"')
 			f_wr.write(result_string+'\n')
